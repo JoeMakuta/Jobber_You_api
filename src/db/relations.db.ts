49c8371610c517784ce4ce5c00d365bcfe83a487
JoeMakuta <makutajosue@gmail.com>
2023-12-01 10:10:25 +0200

    Feat : refac codes

--- src/db/relations.db.ts
+++ src/db/relations.db.ts
@@ -2,8 +2,8 @@ import User from "../models/user.model";
 import Skill from "../models/skill.model";
 import Role from "../models/role.model";
 
-User.belongsToMany(Skill, { through: "UserSkills" });
-Skill.belongsToMany(User, { through: "UserSkills" });
+User.belongsToMany(Skill, { through: "UserSkills", foreignKey: "user_id" });
+Skill.belongsToMany(User, { through: "UserSkills", foreignKey: "skill_id" });
 
-User.belongsToMany(Role, { through: "UserRoles" });
-Role.belongsToMany(User, { through: "UserRoles" });
+User.belongsToMany(Role, { through: "UserRoles", foreignKey: "user_id" });
+Role.belongsToMany(User, { through: "UserRoles", foreignKey: "role_id" });
